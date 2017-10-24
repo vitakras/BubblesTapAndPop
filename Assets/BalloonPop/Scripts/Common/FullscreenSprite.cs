37b825ad8eb4972e7bb3540fdb7781bb88e06fc5
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2017-10-23 20:49:06 -0400

    Added Background Image

---
+++ Assets/BalloonPop/Scripts/Common/FullscreenSprite.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FullscreenSprite : MonoBehaviour {
+
+    void Awake() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+    }
+}
