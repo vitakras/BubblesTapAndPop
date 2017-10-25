d2b6fcdfb9c71acc57fe792771262d0242bec49b
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2017-10-24 20:11:16 -0400

    Update main menu to include bubbles in the main menu

--- Assets/BalloonPop/Scripts/Managers/MainMenu.cs
+++ Assets/BalloonPop/Scripts/Managers/MainMenu.cs
@@ -3,7 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public Spawner spawner;
+    public BubbleColors availableColors;
+
+    private void Awake() {
+        spawner.StartSpawner();
+    }
+
     public void LoadGameLevel() {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void ConfigureBubble(GameObject go) {
+        Bubble bubble = go.GetComponent<Bubble>();
+        if (bubble == null) {
+            return;
+        }
+
+        UpdateBubbleColor(bubble);
+    }
+
+    void UpdateBubbleColor(Bubble bubble) {
+        bubble.Color = availableColors.PickRandomColor();
     }
 }
