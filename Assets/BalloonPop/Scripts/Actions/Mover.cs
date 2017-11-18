c881d75c8554a5d1ec80ada9a1c4263554e395aa
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2017-11-18 11:51:57 -0500

    Change to use rigid body for movement and dissabled game audio

--- Assets/BalloonPop/Scripts/Actions/Mover.cs
+++ Assets/BalloonPop/Scripts/Actions/Mover.cs
@@ -5,9 +5,17 @@ public class Mover : MonoBehaviour {
     public Vector3 direction = Vector3.up;
     public BubbleSpeed bubbleSpeed;
 
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         float speed = this.bubbleSpeed.speed * Time.deltaTime;
-        transform.Translate(direction * speed, Space.World);
+        //transform.Translate(direction * speed, Space.World);
+
+        rb.MovePosition(transform.position + direction * speed);
     }
 }
